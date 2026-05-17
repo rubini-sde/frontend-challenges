1501604106e81bfd2c950f6bde2b5e9dd0a85663
Rubini Dhanasekaran <rubini.sde@gmail.com>
2026-05-17 16:14:26 -0700

    fix some TS errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-/// <reference types="vitest" />
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
-    setupFiles: ["./src/test-setup.ts"],
+    setupFiles: ["./src/test.setup.ts"],
   },
 });
