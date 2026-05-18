40feed812d2d3e1e3b329a596bb435d38f12f995
Rubini Dhanasekaran <rubini.sde@gmail.com>
2026-05-17 17:43:20 -0700

    counterbutton - storybook

---
+++ src/ui/counter-button/CounterButton.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import CounterButton from "./CounterButton";
+
+const meta: Meta<typeof CounterButton> = {
+  component: CounterButton,
+  title: "UI/CounterButton",
+};
+
+export default meta;
+type Story = StoryObj<typeof CounterButton>;
+
+export const Default: Story = {};
+
+export const WithInitialCount: Story = {
+  args: {
+    initialCount: 5,
+  },
+};
