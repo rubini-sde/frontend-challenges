badc7f955ba69e8d46d39a88331d516f34a43f93
Rubini Dhanasekaran <rubini.sde@gmail.com>
2026-05-17 17:54:47 -0700

    add docs to storybook

--- src/ui/counter-button/CounterButton.stories.tsx
+++ src/ui/counter-button/CounterButton.stories.tsx
@@ -4,6 +4,16 @@ import CounterButton from "./CounterButton";
 const meta: Meta<typeof CounterButton> = {
   component: CounterButton,
   title: "UI/CounterButton",
+  tags: ["autodocs"],
+  parameters: {
+    docs: {
+      description: {
+        component: `
+**Challenge:** Build a counter button that increments a count on each click.
+    `,
+      },
+    },
+  },
 };
 
 export default meta;
